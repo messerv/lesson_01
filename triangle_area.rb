b9954747215b0c55fe5b026661a0843b186699c2
Max <messerver@gmail.com>
2022-12-05 11:16:00 +0300

    Added second task

---
+++ triangle_area.rb
@@ -0,0 +1,9 @@
+print "Enter the triangle base length: "
+triangle_base_length = gets.chomp.to_f
+
+print "Enter the triangle height length: "
+triangle_height_length = gets.chomp.to_f
+
+triangle_area = 0.5*triangle_base_length*triangle_height_length
+
+puts "Triangle_area = #{triangle_area.round(2)}"
